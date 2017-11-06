46699421f36e6dbbe12ab04a8575c63fb15140e2
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-11-06 15:15:56 -0500

    Rough colors

--- js/main.js
+++ js/main.js
@@ -8,33 +8,37 @@ function submitdata()
     // console.log(data);
 
     data = data.split('\\\"').join('\"');
+
+    //leading and trailing apostrophes
     data = data.replace("\"{", "{");
     data = data.replace("}\"", "}");
+    data = data.replace("\"[", "[");
+    data = data.replace("]\"", "]");
+
     console.log(data);
 
     var tabs = "";
-
-    var openBracket = "{";
-    var newLine = "\n";
+    var newLine = "<br>";
     var quote = "\"";
 
     var stackBracket = [];
     var stackQuotes = [];
+    var stackColon = [];
 
     for (var index = 0; index < data.length; index++) 
     {
         var character = data.charAt(index);
         
-        if(character == '{') 
+        if(character == '{' || (character == '[')) 
         {
             stackBracket.push(character);
             tabs = tabs.concat("&emsp;");
             result = result.concat(character + newLine + tabs);
-        } else if (character == '}') 
+        } else if (character == '}' || (character == ']')) 
         {
             if (stackBracket.length == 0) 
             {
-                alert('Incorrect format.');
+                alert('Incorrect format. Check if each opening bracket has a closing bracket.');
                 return;
             }
             stackBracket.pop();
@@ -51,16 +55,34 @@ function submitdata()
             result = result.concat(quote);
         } else if (stackQuotes.length == 0 && (character == ' ') || (character == ' \t') || (character == '\n')) {
             //add space for neatness (convert tabs/new lines into empty string)
-            if (data.indexOf(index-1) == ':') 
+            if (result.charAt(result.length-1) == ':') 
             {
-                result.concat(' ');
+                result = result.concat(' ');
             }
-
         } else if (stackQuotes.length == 0 && (character == ',')) {
             result = result.concat(character + newLine + tabs);
+        } else if (character == ":") {
+            if (stackColon.length > 0) {
+                stackColon.push(":");
+            } else {
+                stackColon.pop();
+            }
         } else {
+            if (stackColon.length > 0) {
+                result = result.concat(character);
+            }
             result = result.concat(character);
         }
     }
-    document.getElementById("result").innerHTML = result;
+    if (stackBracket.length > 0 || stackQuotes.length > 0 || stackColon.length > 0) {
+        alert ('Incorrect format. Check for colon, quotes and brackets.');
+        return;
+    }
+
+    var mySpan = document.createElement('span');
+    mySpan.innerHTML = result;
+    mySpan.style.color = "#ff0000";
+    document.getElementById('myContainer').appendChild(mySpan);
+
+//    document.getElementById("result").innerHTML = result;
 }
